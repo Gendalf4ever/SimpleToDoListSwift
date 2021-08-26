25719a46e909a710d6885e6d07e285e2f3da5bd3
Gendalf4ever <60482216+Gendalf4ever@users.noreply.github.com>
2021-08-26 16:25:17 +0300

    База приложения

---
+++ ToDOList/Model.swift
@@ -0,0 +1,29 @@
+//
+//  Model.swift
+//  ToDOList
+//
+//  Created by Master on 26.08.2021.
+//  Copyright © 2021 Master. All rights reserved.
+//
+
+import Foundation
+
+
+var ToDoItems: [[String: Any]] = [["Name" : "bebe", "isCompleted": false], ["Name" : "baba", "isCompleted": false], ["Name" : "bubu", "isCompleted": false]]
+func addItem(itemName:String, isCompleted: Bool = false){
+    ToDoItems.append(["Name": itemName, "isComleted": false])
+    saveData()
+}
+
+func removeItem(at index: Int) {
+    ToDoItems.remove(at: index)
+    saveData()
+}
+
+func saveData() {
+    
+}
+
+func loadData() {
+
+}
